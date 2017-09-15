81a4b360d73ba1ac2f94b6b774f3f090a4c8acb1
Alex Regan <alex.joseph.regan@gmail.com>
2017-09-15 14:25:44 -0700

    fix(check-mark): remove unnecessary xml attrs

--- src/components/CheckMark.js
+++ src/components/CheckMark.js
@@ -13,34 +13,21 @@ export default {
     }
   },
   render(h, { props, data }) {
-    return h(
-      "svg",
-      mergeData(data, {
+    return h("svg", mergeData(data, { attrs: { viewBox: "0 0 64 64" } }), [
+      h("circle", {
         attrs: {
-          "enable-background": "new 0 0 64 64",
-          version: "1.1",
-          viewBox: "0 0 64 64",
-          "xml:space": "preserve",
-          xmlns: "http://www.w3.org/2000/svg",
-          "xmlns:xlink": "http://www.w3.org/1999/xlink"
+          cx: "32",
+          cy: "32",
+          r: "32",
+          fill: props.color
         }
       }),
-      [
-        h("circle", {
-          attrs: {
-            cx: "32",
-            cy: "32",
-            r: "32",
-            fill: props.color
-          }
-        }),
-        h("polygon", {
-          attrs: {
-            fill: props.fill,
-            points: "43.266,18.345 27.915,37 21.465,30.725 17.211,35.34 28.413,46.236 47.989,22.449"
-          }
-        })
-      ]
-    )
+      h("polygon", {
+        attrs: {
+          fill: props.fill,
+          points: "43.266,18.345 27.915,37 21.465,30.725 17.211,35.34 28.413,46.236 47.989,22.449"
+        }
+      })
+    ])
   }
 }
