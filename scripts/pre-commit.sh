d1c481da845d611e68beb02796fec6e9a6d41360
Alex Regan <alex.joseph.regan@gmail.com>
2018-03-25 20:58:34 -0700

    chore: fixes prettierignore paths

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -2,4 +2,4 @@
 
 PRETTIER=$PWD/node_modules/.bin/prettier
 
-$PRETTIER --config $PWD/.prettierrc --ignore $PWD/.prettierignore --write $PWD/**/*.{js,ts,md}
+$PRETTIER --config $PWD/.prettierrc --ignore-path $PWD/.prettierignore --write $PWD/src/**/*.{js,ts} $PWD/*.{md,ts,js}
